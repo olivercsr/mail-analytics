7b74d526e73246fad286cbab86331e0c47e055ff
Oliver Wegner <oliver.wegner@csr-informatik.de>
2024-03-05 19:44:37 +0100

    start implementing dmarc structs

---
+++ src/dmarc.rs
@@ -0,0 +1,33 @@
+enum Adkim {
+    Relaxed,
+    Strict
+}
+
+enum Aspf {
+    Relaxed,
+    Strict
+}
+
+enum Action {
+    None,
+    Quarantine,
+    Reject
+}
+
+struct Policy {
+    domain: String,
+    adkim: Adkim,
+    aspf: Aspf,
+    percentage: u8,
+    action: Action
+}
+
+struct Sender {
+    host: String
+}
+
+struct Result {
+    sender: Sender,
+    dkim_pass: bool,
+    spf_pass: bool
+}
