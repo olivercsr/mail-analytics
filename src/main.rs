13208ae4c30c6ed3c8a9892aacecb9686ec1ab08
Oliver Wegner <oliver.wegner@csr-informatik.de>
2024-03-25 20:26:00 +0100

    adding tokio runtime

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::fs::File;
 use mail_analytics::{add, do_xml};
 
-fn main() {
+#[tokio::main]
+async fn main() {
     println!("Hello, world: {}+{}={}!", 33, 44, add(33, 44));
 
     let file = File::open("./dmarc/google.com!csr-informatik.de!1707782400!1707868799.xml").unwrap();
