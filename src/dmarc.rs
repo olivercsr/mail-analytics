bb7b19d4c0a7aeb2f6325d2a3fbb835bfa3bb4a9
Oliver Wegner <oliver.wegner@csr-informatik.de>
2024-03-25 22:46:14 +0100

    work on dmarc structs

--- src/dmarc.rs
+++ src/dmarc.rs
@@ -1,63 +1,91 @@
 use chrono::{DateTime, Utc};
 
+#[derive(Debug)]
 pub enum Adkim {
     Relaxed,
     Strict
 }
 
+#[derive(Debug)]
 pub enum Aspf {
     Relaxed,
     Strict
 }
 
-pub enum Action {
-    None,
-    Quarantine,
-    Reject
+#[derive(Debug)]
+pub struct Dkim {
+    pub domain: String,
+    pub result: bool,
+    pub selector: Option<String>
 }
 
-pub struct Policy {
+#[derive(Debug)]
+pub struct Spf {
+    pub domain: String,
+    pub result: bool
+}
+
+#[derive(Debug)]
+pub struct AuthResults {
+    pub dkim: Dkim,
+    pub spf: Spf
+}
+
+#[derive(Debug)]
+pub struct Identifiers {
+    pub envelope_from: String,
+    pub header_from: String
+}
+
+#[derive(Debug)]
+pub struct PolicyEvaluated {
+    disposition: String,
+    dkim: bool,
+    spf: bool
+}
+
+#[derive(Debug)]
+pub struct Row {
+    source_id: String,
+    count: usize,
+    policy_evaluated: PolicyEvaluated
+}
+
+#[derive(Debug)]
+pub struct Record {
+    pub rows: Vec<Row>,
+    pub identifiers: Identifiers,
+    pub auth_results: AuthResults
+}
+
+#[derive(Debug)]
+pub struct PolicyPublished {
     pub domain: String,
     pub adkim: Adkim,
     pub aspf: Aspf,
+    pub p: (), // TODO
+    pub sp: (), // TODO
     pub percentage: u8,
-    pub action: Action
+    pub fo: () // TODO
 }
 
-pub struct Mailer {
-    pub host: String
+#[derive(Debug)]
+pub struct DateRange {
+    pub begin: DateTime<Utc>,
+    pub end: DateTime<Utc>
 }
 
-pub struct DkimResult {
-    pub domain: String,
-    pub selector: String,
-    pub result: bool
-}
-
-pub struct SpfResult {
-    pub domain: String,
-    pub result: bool
-}
-
-pub struct Result {
-    pub mailer: Mailer,
-    pub header_from: String,
-    pub dkim: DkimResult,
-    pub spf: SpfResult,
-    pub count: usize
-}
-
-pub struct Reporter {
-    pub name: String,
+#[derive(Debug)]
+pub struct ReportMetadata {
+    pub report_id: String,
+    pub ord_name: String,
     pub email: String,
-    pub info: String
+    pub date_range: DateRange
 }
 
 #[derive(Debug)]
 pub struct Report {
-    //reporter: Reporter,
-    pub id: String,
-    pub start: DateTime<Utc>,
-    pub end: DateTime<Utc>,
-    //policy: Policy
+    pub report_metadata: ReportMetadata,
+    //pub policy_published: PolicyPublished,
+    //pub records: Vec<Record>,
 }
