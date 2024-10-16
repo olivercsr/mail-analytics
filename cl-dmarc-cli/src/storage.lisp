a79f501c05be5040ee9aac0b2fbbba0142b37716
Oliver Wegner <oliver.wegner@csr-informatik.de>
2024-10-16 15:47:09 +0200

    working on storage

--- cl-dmarc-cli/src/storage.lisp
+++ cl-dmarc-cli/src/storage.lisp
@@ -1,3 +1,5 @@
 (in-package :cl-dmarc-cli)
 
-(defgeneric store-report (storage report))
+(defgeneric store-report (storage id report))
+(defgeneric get-report (storage id))
+(defgeneric find-reports-by-daterange (storage start-date end-date))
--- cl-dmarc-cli/src/storages/memory.lisp
+++ cl-dmarc-cli/src/storages/memory.lisp
@@ -1,6 +1,21 @@
 (in-package :cl-dmarc-cli)
 
-(defclass memory-storage () ())
+(defclass memory-storage () ((htable :initform (make-hash-table))))
 
-(defmethod store-report ((storage memory-database) report)
-  (format t "memory-storage ~a~%" report))
+(defmethod store-report ((storage memory-database) id report)
+  (setf (gethash id report)
+        report))
+
+(defmethod get-report ((storage memory-database) id)
+  (gethash id report))
+
+(defmethod find-reports-by-daterange ((storage memory-storage) start-date end-date)
+  nil)
+
+
+;;(let ((ht (make-hash-table)))
+;;  (setf (gethash :foo ht) 123.0)
+;;  (gethash :foo ht))
+
+;;(let ((s (make-instance 'memory-storage)))
+;;  (format t "memory-storage: ~a~%" (slot-value s 'htable)))
