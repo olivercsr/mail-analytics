63355f58ed7c22d2f19064cbff360792f2733c8c
Oliver Wegner <oliver.wegner@csr-informatik.de>
2024-10-16 14:19:59 +0200

    work on find-nodes

--- cl-dmarc-cli/src/read-xml.lisp
+++ cl-dmarc-cli/src/read-xml.lisp
@@ -2,19 +2,19 @@
 
 ;;(declaim (optimize (speed 0) (space 0) (debug 3)))
 
-(defun find-nodes (nodes &key (wanted-p #'identity) (node-children #'cdr) (results nil))
+(defun find-nodes (nodes &key (wanted-p #'identity) (children #'cdr) (results nil))
   ;;(break)
   (if nodes
       (let* ((node       (car nodes))
              (rest-nodes (cdr nodes)))
         (find-nodes (concatenate 'list
                                  rest-nodes
-                                 (funcall node-children node))
+                                 (funcall children node))
                      :results (if (funcall wanted-p node)
                                   (cons node results)
                                   results)
                      :wanted-p wanted-p
-                     :node-children node-children))
+                     :children children))
       results))
 
 ;;(defun find-nodes (node value &key (node-p #'identity) (node-value #'car) (node-children #'cdr))
@@ -29,8 +29,12 @@
 ;;                  (find-nodes node value :node-p node-p :node-value node-value :node-children node-children))
 ;;              (funcall node-children node))))))
 
-(defun access-node (node name)
-  (find-nodes #'x:node-p #'x:node-name #'x:node-children node name))
+(defun access-nodes (nodes name)
+  (find-nodes nodes
+              :wanted-p #'(lambda (node)
+                            (equal (node-name node)
+                                   name))
+              :children #'x:node-children))
 
 (defun parse-xml (stream)
   (x:parse stream))
@@ -38,7 +42,9 @@
 
 
 (find-nodes '((11 ((22) (33) (44 ((55) (99) (33 ((44) (33))))) (33 ((11))))))
-            :wanted-p #'(lambda (node) (eql (car node) 33))
+            :wanted-p #'(lambda (node)
+                          (eql (car node)
+                               33))
             ;;:node-p #'identity
             ;;:node-value #'car
             ;;:node-children #'cdr
