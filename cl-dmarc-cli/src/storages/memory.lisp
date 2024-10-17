8df45de10952b6617f8a379cd2f4e5e2b82ed367
Oliver Wegner <oliver.wegner@csr-informatik.de>
2024-10-17 17:20:13 +0200

    working on storage

--- cl-dmarc-cli/src/storages/memory.lisp
+++ cl-dmarc-cli/src/storages/memory.lisp
@@ -2,12 +2,12 @@
 
 (defclass memory-storage () ((htable :initform (make-hash-table))))
 
-(defmethod store-report ((storage memory-database) id report)
-  (setf (gethash id report)
+(defmethod store-report ((storage memory-storage) id report)
+  (setf (gethash id (slot-value storage 'htable))
         report))
 
-(defmethod get-report ((storage memory-database) id)
-  (gethash id report))
+(defmethod get-report ((storage memory-storage) id)
+  (gethash id (slot-value storage 'htable)))
 
 (defmethod find-reports-by-daterange ((storage memory-storage) start-date end-date)
   nil)
@@ -17,5 +17,9 @@
 ;;  (setf (gethash :foo ht) 123.0)
 ;;  (gethash :foo ht))
 
-;;(let ((s (make-instance 'memory-storage)))
-;;  (format t "memory-storage: ~a~%" (slot-value s 'htable)))
+(let ((s (make-instance 'memory-storage)))
+  (store-report s :aaa (list 11 22 33))
+  (store-report s :bbb (list 22 33 44))
+  (format t "memory-storage: ~a~%~a~%"
+          (slot-value s 'htable)
+          (get-report s :bbb)))
--- cl-dmarc-cli/src/storages/postgres.lisp
+++ cl-dmarc-cli/src/storages/postgres.lisp
@@ -2,5 +2,5 @@
 
 (defclass postgres-storage () (pg-client))
 
-(defmethod store-report ((db postgres-storage) report)
-  (format t "postgres-storage ~a~%" report))
+(defmethod store-report ((db postgres-storage) id report)
+  (format t "postgres-storage ~a ~a~%" id report))
