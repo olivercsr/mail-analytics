421f729dc2f14029db31debfa617b04967c0d9a7
Oliver Wegner <oliver.wegner@csr-informatik.de>
2024-10-18 16:45:50 +0200

    change find-nodes to traverse-nodes,
    featuring an action instead of wanted-p, plus recording parents by
    traversal path and submitting those to the action fn.

--- cl-dmarc-cli/src/read-xml.lisp
+++ cl-dmarc-cli/src/read-xml.lisp
@@ -2,51 +2,50 @@
 
 ;;(declaim (optimize (speed 0) (space 0) (debug 3)))
 
-(defun find-nodes (nodes
-                   &key (node-p #'identity)
-                     (wanted-p #'identity)
-                     (children #'cdr)
-                     (results nil))
+(defun traverse-nodes (nodes
+                       &key (node-p #'identity)
+                         (action #'(lambda (&rest args)
+                                     (declare (ignore args))))
+                         (children #'cdr)
+                         (parents '()))
   (let ((node       (car nodes))
         (rest-nodes (cdr nodes)))
-    (if (funcall node-p node)
-        (find-nodes (concatenate 'list
-                                 rest-nodes
-                                 (funcall children node))
-                    :results  (if (funcall wanted-p node)
-                                  (cons node results)
-                                  results)
-                    :node-p   node-p
-                    :wanted-p wanted-p
-                    :children children)
-        results)))
-
-;;(defun find-nodes (node value &key (node-p #'identity) (node-value #'car) (node-children #'cdr))
-;;  (when (funcall node-p node)
-;;    (mapcan
-;;     #'identity
-;;     (cons
-;;      (when (equal (funcall node-value node)
-;;                   value)
-;;        (list node))
-;;      (mapcar #'(lambda (node)
-;;                  (find-nodes node value :node-p node-p :node-value node-value :node-children node-children))
-;;              (funcall node-children node))))))
+    (when (funcall node-p node)
+      (funcall action node parents)
+      (traverse-nodes (concatenate 'list
+                                   rest-nodes
+                                   (funcall children node))
+                      :action   action
+                      :node-p   node-p
+                      :children children
+                      :parents  (cons node parents)))))
 
 (defun report-metadata-node (xml)
-  (car (find-nodes (list xml)
-                   :node-p   #'x:node-p
-                   :wanted-p #'(lambda (node)
-                                 (equal (x:node-name node)
-                                        "report_metadata"))
-                   :children #'x:node-children)))
+  (car (traverse-nodes (list xml)
+                       :node-p   #'x:node-p
+                       :wanted-p #'(lambda (node)
+                                     (equal (x:node-name node)
+                                            "report_metadata"))
+                       :children #'x:node-children)))
 
 (defun parse-xml (stream)
   (x:parse stream))
 
 
 
-(find-nodes '((11 ((22) (33) (44 ((55) (99) (33 ((44) (33))))) (33 ((11))))))
-            :wanted-p #'(lambda (node)
-                          (eql (car node)
-                               33)))
+;;(let ((policies-evaluated '()))
+;;  (traverse-nodes '((11 ((22) (33) (44 ((55) (99) (33 ((44) (33))))) (33 ((11))))))
+;;                  :action #'(lambda (node parents)
+;;                              ;;(format t "node: ~a parents: ~a~%" node parents)
+;;                              (when (equal (car node) 33)
+;;                                (setf policies-evaluated
+;;                                      (cons (car node)
+;;                                            policies-evaluated)))))
+;;  policies-evaluated)
+
+;;(let ((al nil))
+;;  (setf al (acons :x 11 al))
+;;  (setf al (acons :y 22 al))
+;;  (list al
+;;        (assoc :y al)
+;;        (rassoc 11 al)))
