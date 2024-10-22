3415a968c36d4bfa15dcc27d0d2d29a2039bcb81
Oliver Wegner <oliver.wegner@csr-informatik.de>
2024-10-22 19:52:17 +0200

    working on sql

--- cl-dmarc-cli/sql/00-init.sql
+++ cl-dmarc-cli/sql/00-init.sql
@@ -1,3 +1,7 @@
+drop table if exists report_record;
+drop table if exists report;
+drop table if exists reporter;
+
 create table reporter (
   "id" serial not null,
   "name" text,
@@ -10,8 +14,8 @@ create table report (
   "id" serial not null,
   "reporter_id" int,
   "identifier" text,
-  "start" timestamptz,
-  "end" timestamptz,
+  "starttime" timestamptz,
+  "endtime" timestamptz,
 
   primary key ("id"),
   foreign key ("reporter_id") references reporter ("id") on update cascade on delete cascade
@@ -32,10 +36,19 @@ create table policy (
 */
 
 create table report_record (
-  "time" timestamptz not null,
+  "starttime" timestamptz not null,
+  "endtime" timestamptz not null,
   "report_id" int,
+  "policy_adkim" text,
+  "policy_aspf" text,
+  "policy_p" text,
+  "policy_sp" text,
+  "policy_pct" int,
+  "policy_fo" int,
+
   /* TODO */
 
   foreign key ("report_id") references report ("id") on update cascade on delete cascade
 );
-select create_hypertable('report_record', 'time');
+select create_hypertable('report_record', 'starttime');
+create index report_record_end_idx on report_record ("endtime");
