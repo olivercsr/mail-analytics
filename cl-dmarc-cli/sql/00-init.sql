cae95f48c9be75ebea41a61d43de4e823738726f
Oliver Wegner <oliver.wegner@csr-informatik.de>
2024-11-04 17:58:44 +0100

    working on sql schema

--- cl-dmarc-cli/sql/00-init.sql
+++ cl-dmarc-cli/sql/00-init.sql
@@ -1,6 +1,9 @@
-/*drop table if exists report_record;*/
+drop table if exists spf_evaulation;
+drop table if exists dkim_evaulation;
+drop table if exists evaulation;
 drop table if exists report;
 drop table if exists reporter;
+
 
 create type alignment as enum ('strict', 'relaxed');
 create type disposition as enum ('none', 'quarantine', 'reject');
@@ -9,6 +12,7 @@ create type policy_override as enum ('forwarded', 'sampled_out', 'trusted_forwar
 create type dkim_result as enum ('none', 'pass', 'fail', 'policy', 'neutral', 'temperror', 'permerror');
 create type spf_domain_scope as enum ('helo', 'mfrom');
 create type spf_result as enum ('none', 'neutral', 'pass', 'fail', 'softfail', 'temperror', 'permerror');
+
 
 create table reporter (
   "id" serial not null,
@@ -19,6 +23,7 @@ create table reporter (
 
   primary key ("id")
 );
+
 
 create table report (
   "report_id" text not null,
@@ -42,13 +47,13 @@ create table report (
 select create_hypertable('report', 'begin');
 create index report_end_idx on report ("end");
 
-create table policy_evaluated (
-  "begin" timestamptz not null,
-  "end" timestamptz not null,
+
+create table evaluation (
+  "id" serial not null,
   "report_id" text,
 
   /* record.row */
-  "source_ip" text,
+  "source_ip" inet,
   "count" int,
 
   /* record.row.policy_evaluated */
@@ -61,31 +66,32 @@ create table policy_evaluated (
   "envelope_to" text,
   "header_from" text,
 
-  /* record.auth_results */
-
-  /* TODO */
-
+  primary key ("id"),
   foreign key ("report_id") references report ("report_id") on update cascade on delete cascade
 );
-select create_hypertable('policy_evaluated', 'begin');
-create index policy_evaluated_end_idx on policy_evaluated ("end");
 
-create table report_dkim_evaluation (
-  "starttime" timestamptz not null,
-  "endtime" timestamptz not null,
+
+create table dkim_evaluation (
+  "id" serial not null,
+  "evaluation_id" int,
 
   "domain" text,
   "selector" text,
-  "result" text
-);
-select create hypertable('report_dkim_evaluation', 'startime');
+  "result" text,
 
-create table report_spf_evaluation (
-  "startime" timestamptz not null,
-  "endtime" timestamptz not null,
+  primary key ("id"),
+  foreign key ("evaluation_id") references evaluation ("id") on update cascade on delete cascade
+);
+
+
+create table spf_evaluation (
+  "id" serial not null,
+  "evaluation_id" int,
 
   "domain" text,
   "scope" text,
-  "result" text
+  "result" text,
+
+  primary key ("id"),
+  foreign key ("evaluation_id") references evaluation ("id") on update cascade on delete cascade
 );
-select create hypertable('report_spf_evaluation', 'startime');
