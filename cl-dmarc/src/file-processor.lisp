efe01c2b4f61e8d399adf6b26afee892c4178584
Oliver Wegner <oliver.wegner@csr-informatik.de>
2024-12-05 15:27:09 +0100

    working on file-processor

--- cl-dmarc/src/file-processor.lisp
+++ cl-dmarc/src/file-processor.lisp
@@ -2,4 +2,5 @@
 
 (defgeneric lock-next-file (file-processor))
 (defgeneric file-done (file-processor file))
+(defgeneric archive-done-files (file-processor))
 (defgeneric reset-stuck-files (file-processor))
--- cl-dmarc/src/file-processors/filesystem.lisp
+++ cl-dmarc/src/file-processors/filesystem.lisp
@@ -5,18 +5,27 @@
               :initarg :base-path)))
 
 (defmethod lock-next-file ((file-processor filesystem-file-processor))
-  (shared:traverse-nodes (list (slot-value file-processor 'base-path))
-                         :action #'(lambda (path &rest args)
-                                     (declare (ignore args))
-                                     (uiop:directory-files (truename path)
-                                                           "*.xml"))
-                         :children #'(lambda (path)
-                                       (uiop:subdirectories (truename path)))
-                         :merge-results #'(lambda (new-results results)
-                                            (concatenate 'list
-                                                         new-results
-                                                         results))))
+  (let ((files (shared:traverse-nodes (list (slot-value file-processor 'base-path))
+                                      :action #'(lambda (path &rest args)
+                                                  (declare (ignore args))
+                                                  (uiop:directory-files (truename path)
+                                                                        "*.xml"))
+                                      :children #'(lambda (path)
+                                                    (uiop:subdirectories (truename path)))
+                                      :merge-results #'(lambda (new-results results)
+                                                         (concatenate 'list
+                                                                      new-results
+                                                                      results)))))
+    ))
 
+(defmethod file-done ((file-processor filesystem-file-processor) file)
+  (format t "file-done ~a~%" file))
+
+(defmethod archive-done-files ((file-processor filesystem-file-processor))
+  (format t "archive-done-files~%"))
+
+(defmethod reset-stuck-files ((file-processor filesystem-file-processor))
+  (format t "reset-stuck-files~%"))
 
 ;;(let ((ffp (make-instance 'filesystem-file-processor
 ;;                          :base-path #p".")))
