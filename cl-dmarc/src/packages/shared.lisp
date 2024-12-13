c3624fb54bce6342e9a432a8f2963c8cf577e8f9
Oliver Wegner <oliver.wegner@csr-informatik.de>
2024-12-13 17:00:23 +0100

    fix constructor exports

--- cl-dmarc/src/packages/shared.lisp
+++ cl-dmarc/src/packages/shared.lisp
@@ -2,14 +2,13 @@
   (:use :cl :arrow-macros)
   (:export :traverse-nodes
 
-   :make-reporter
+   :reporter
+   :report
+   :evaluation
+   :dkim-evaluation
+   :spf-evaluation
+
            :reporter-id
            :reporter-org-name
            :reporter-email
-           :reporter-extra-contact-info
-
-           :make-report
-           :make-evaluation
-           :make-dkim-evaluation
-           :make-spf-evaluation
-           ))
+           :reporter-extra-contact-info))
