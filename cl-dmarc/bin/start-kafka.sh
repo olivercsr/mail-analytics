aeb926cdc21a38f03123849418727667963c599a
Oliver Wegner <oliver.wegner@csr-informatik.de>
2024-12-13 15:43:10 +0100

    introduce start-kafka.sh

---
+++ cl-dmarc/bin/start-kafka.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+DOCKER_CMD=
+if [ -x "$(command -v podman)" ]; then
+  DOCKER_CMD="$(command -v podman)"
+elif [ -x "$(command -v docker)" ]; then
+  DOCKER_CMD="$(command -v docker)"
+else
+  echo "ERROR: could not find either docker or podman" >&2
+  exit 1
+fi
+
+KAFKA_NAME=${KAFKA_NAME:-kafka-dev}
+
+${DOCKER_CMD} run -it --rm \
+  --name "${KAFKA_NAME}" \
+  --hostname "${KAFKA_NAME}" \
+  -m 256m \
+  -e KAFKA_CFG_NODE_ID=0 \
+  -e KAFKA_CFG_PROCESS_ROLES=controller,broker \
+  -e KAFKA_CFG_LISTENERS=PLAINTEXT://:9092,CONTROLLER://:9093 \
+  -e KAFKA_CFG_LISTENER_SECURITY_PROTOCOL_MAP=CONTROLLER:PLAINTEXT,PLAINTEXT:PLAINTEXT \
+  -e KAFKA_CFG_CONTROLLER_QUORUM_VOTERS=0@"${KAFKA_NAME}":9093 \
+  -e KAFKA_CFG_CONTROLLER_LISTENER_NAMES=CONTROLLER \
+  -p 9092:9092 \
+  -p 9093:9093 \
+  docker.io/bitnami/kafka
