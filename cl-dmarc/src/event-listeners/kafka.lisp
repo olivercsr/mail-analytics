49f58df6dbbf1ab735dd2b500dad791787c43abc
Oliver Wegner <oliver.wegner@csr-informatik.de>
2024-12-21 15:10:20 +0100

    change kafka code back from debugging to "normal" state

--- cl-dmarc/src/event-listeners/kafka.lisp
+++ cl-dmarc/src/event-listeners/kafka.lisp
@@ -11,30 +11,31 @@
    (consumer)))
 
 (defmethod connect ((event-listener kafka-event-listener))
-  (let* (;;(consumer (make-instance 'kf:consumer
-         ;;                         :conf '("bootstrap.servers" "127.0.0.1:9092"
-         ;;                                 "group.id" "dmarc-importer"
-         ;;                                 "enable.auto.commit" "false"
-         ;;                                 "auto.offset.reset" "earliest"
-         ;;                                 "allow.auto.create.topics" "true")
-         ;;                         :serde #'babel:octets-to-string
-         ;;                         :key-serde 123
-         ;;                         :value-serde 234))
-         (conf (cl-rdkafka/ll:rd-kafka-conf-new))
-         (errstrlen 8192)
-         (consumer (cffi:with-foreign-object (errstr :char errstrlen)
-                     (cl-rdkafka/ll:rd-kafka-new cl-rdkafka/ll:rd-kafka-consumer
-                                                 ;;(cffi:null-pointer)
-                                                 conf
-                                                 errstr
-                                                 errstrlen)))
+  (let* ((consumer (make-instance 'kf:consumer
+                                  :conf '("bootstrap.servers" "127.0.0.1:9092"
+                                          "group.id" "dmarc-importer"
+                                          "enable.auto.commit" "false"
+                                          "auto.offset.reset" "earliest"
+                                          "allow.auto.create.topics" "true")
+                                  :serde #'babel:octets-to-string
+                                  ;;:key-serde 123
+                                  ;;:value-serde 234
+                                  ))
+         ;;(conf (cl-rdkafka/ll:rd-kafka-conf-new))
+         ;;(errstrlen 8192)
+         ;;(consumer (cffi:with-foreign-object (errstr :char errstrlen)
+         ;;            (cl-rdkafka/ll:rd-kafka-new cl-rdkafka/ll:rd-kafka-consumer
+         ;;                                        ;;(cffi:null-pointer)
+         ;;                                        conf
+         ;;                                        errstr
+         ;;                                        errstrlen)))
          )
-    (break)
-    (format t "=========================== ~a ~a~%" conf errstr)
-    (format t "CONNECT~%")
-    ;;(kf:subscribe consumer "dmarc-file-received")
-    ;;(setf (slot-value event-listener 'consumer)
-    ;;      consumer)
+    ;;(break)
+    ;;(format t "=========================== ~a ~a~%" conf consumer)
+    (format t "CONNECT ~a~%" consumer)
+    (kf:subscribe consumer "dmarc-file-received")
+    (setf (slot-value event-listener 'consumer)
+          consumer)
     event-listener))
 
 (defmethod disconnect ((event-listener kafka-event-listener))
