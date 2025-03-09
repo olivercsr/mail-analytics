bcc0bdddb4c571a52d33aaa6a63c7cdcd09bb945
Oliver Wegner <oliver.wegner@csr-informatik.de>
2025-03-09 20:54:27 +0100

    working on mime part processing with example

--- cl-dmarc/src/mail-processor.lisp
+++ cl-dmarc/src/mail-processor.lisp
@@ -1,14 +1,39 @@
 (in-package :mail-processor)
 
 (defun process-mail (mail)
-  (format t "processing mail ~a~%" mail)
+  ;;(format t "processing mail ~a~%" mail)
   (let ((mime (mi:parse-mime mail)))
     (case (type-of mime)
-      (mi:text-mime (print *standard-output* (mi:content mime)))
+      (mi:text-mime (print (mi:content mime)))
       (mi:multipart-mime
-       (dolist (part (mi:content mime))
-         (if (and (string-equal (mi:content-type part) "text")
-                  (string-equal (mi:content-subtype part) "plain"))
-             (mi:print-mime *standard-output* part nil nil))))
+       (progn
+         (format t "multipart~%")
+         (dolist (part (mi:content mime))
+           (if (and (string-equal (mi:content-type part) "text")
+                    (string-equal (mi:content-subtype part) "plain"))
+               (mi:print-mime *standard-output* part nil nil)))))
       (mi:mime (print "mime"))
-      (t (print "other")))))
+      (t (print "other")))
+    ))
+
+
+(process-mail "From: Some One <someone@example.com>
+MIME-Version: 1.0
+Content-Type: multipart/mixed;
+        boundary=\"XXXXboundary text\"
+
+This is a multipart message in MIME format.
+
+--XXXXboundary text
+Content-Type: text/plain
+
+this is the body text
+
+--XXXXboundary text
+Content-Type: text/plain;
+Content-Disposition: attachment;
+        filename=\"test.txt\"
+
+this is the attachment text
+
+--XXXXboundary text--")
