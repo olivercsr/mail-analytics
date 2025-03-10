166dcca2e8e1f19a0327ba056a2f434e267bdcfa
Oliver Wegner <oliver.wegner@csr-informatik.de>
2025-03-10 16:12:19 +0100

    working on mime part handling

--- cl-dmarc/src/mail-processor.lisp
+++ cl-dmarc/src/mail-processor.lisp
@@ -10,45 +10,24 @@
     (t (with-input-from-string (content (mi:content mime))
          (uiop:copy-stream-to-stream content out-stream)))))
 
-(defun process-part (mime)
+(defun process-part (mime make-output-stream)
   (format t "process-part enter ~a ~a (~a/~a) ~a ~a~%"
           (mi:content-disposition mime) (mi:content-disposition-parameters mime)
           (mi:content-type mime) (mi:content-subtype mime)
           (mi:content-transfer-encoding mime) (type-of (mi:content mime)))
   (case (type-of mime)
     (mi:multipart-mime (dolist (part (mi:content mime))
-                         (process-part part)))
+                         (process-part part make-output-stream)))
     (t (a:switch ((mi:content-disposition mime) :test #'equal)
-         ("attachment" (format t "OUT: ~a~%"
-                               (length (with-output-to-string (out)
-                                         (decode-part mime out))))
-                       ;;(a:switch ((mi:content-subtype mime) :test #'equal)
-                       ;;  ("gzip" (format t "found gzip mime-part~%"))
-                       ;;  ("zip"  (format t "found zip mime-part~%")))
-                       ))))
-  ;;(case (type-of mime)
-  ;;  ;;(mi:text-mime (print (mi:content mime)))
-  ;;  (mi:text-mime (format t "text-mime~%"))
-  ;;  (mi:multipart-mime
-  ;;   (progn
-  ;;     ;;(format t "multipart~%")
-  ;;     (dolist (part (mi:content mime))
-  ;;       ;;(format t "part ~a/~a~%" (mi:content-type part) (mi:content-subtype part))
-  ;;       (process-part part)
-  ;;       ;;(if (and (string-equal (mi:content-type part) "text")
-  ;;       ;;         (string-equal (mi:content-subtype part) "plain"))
-  ;;       ;;    (mi:print-mime *standard-output* part nil nil))
-  ;;       )))
-  ;;  (mi:mime (format t "mime~%"))
-  ;;  (t (format t "other~%")))
-  )
+         ("attachment" (decode-part mime (funcall make-output-stream)))))))
 
-(defun process-mail (mail)
+(defun process-mail (mail make-output-stream)
   ;;(format t "processing mail ~a~%" mail)
   (let ((mime (mi:parse-mime mail)))
-    (process-part mime)))
+    (process-part mime make-output-stream)))
 
 
 (with-open-file (in #p"../../../dmarc-data/mails/mail01.eml"
                     :direction :input)
-  (process-mail in))
+  (process-mail in #'(lambda ()
+                       *standard-output*)))
