0e7e30d84463abb393c3ccdf5c2d7657bea074ba
Oliver Wegner <oliver.wegner@csr-informatik.de>
2025-03-10 13:25:08 +0100

    working on mime mail decoder/processor logic

--- cl-dmarc/src/mail-processor.lisp
+++ cl-dmarc/src/mail-processor.lisp
@@ -1,39 +1,38 @@
 (in-package :mail-processor)
 
+(defun process-part (mime)
+  (format t "process-part enter (~a/~a)~%"
+          (mi:content-type mime) (mi:content-subtype mime))
+  (case (type-of mime)
+    (mi:multipart-mime (dolist (part (mi:content mime))
+                         (process-part part)))
+    (t (a:switch ((mi:content-type mime) :test #'equal)
+         ("application" (a:switch ((mi:content-subtype mime) :test #'equal)
+                          ("gzip" (format t "found gzip mime-part~%"))
+                          ("zip"  (format t "found zip mime-part~%")))))))
+  ;;(case (type-of mime)
+  ;;  ;;(mi:text-mime (print (mi:content mime)))
+  ;;  (mi:text-mime (format t "text-mime~%"))
+  ;;  (mi:multipart-mime
+  ;;   (progn
+  ;;     ;;(format t "multipart~%")
+  ;;     (dolist (part (mi:content mime))
+  ;;       ;;(format t "part ~a/~a~%" (mi:content-type part) (mi:content-subtype part))
+  ;;       (process-part part)
+  ;;       ;;(if (and (string-equal (mi:content-type part) "text")
+  ;;       ;;         (string-equal (mi:content-subtype part) "plain"))
+  ;;       ;;    (mi:print-mime *standard-output* part nil nil))
+  ;;       )))
+  ;;  (mi:mime (format t "mime~%"))
+  ;;  (t (format t "other~%")))
+  )
+
 (defun process-mail (mail)
   ;;(format t "processing mail ~a~%" mail)
   (let ((mime (mi:parse-mime mail)))
-    (case (type-of mime)
-      (mi:text-mime (print (mi:content mime)))
-      (mi:multipart-mime
-       (progn
-         (format t "multipart~%")
-         (dolist (part (mi:content mime))
-           (if (and (string-equal (mi:content-type part) "text")
-                    (string-equal (mi:content-subtype part) "plain"))
-               (mi:print-mime *standard-output* part nil nil)))))
-      (mi:mime (print "mime"))
-      (t (print "other")))
-    ))
+    (process-part mime)))
 
 
-(process-mail "From: Some One <someone@example.com>
-MIME-Version: 1.0
-Content-Type: multipart/mixed;
-        boundary=\"XXXXboundary text\"
-
-This is a multipart message in MIME format.
-
---XXXXboundary text
-Content-Type: text/plain
-
-this is the body text
-
---XXXXboundary text
-Content-Type: text/plain;
-Content-Disposition: attachment;
-        filename=\"test.txt\"
-
-this is the attachment text
-
---XXXXboundary text--")
+(with-open-file (in #p"../../../dmarc-data/mails/mail01.eml"
+                    :direction :input)
+  (process-mail in))
