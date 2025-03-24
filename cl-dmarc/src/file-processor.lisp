758d26b06b934e81de572fbdadb02b871860965b
Oliver Wegner <oliver.wegner@csr-informatik.de>
2025-03-24 13:44:56 +0100

    start working on file-processor

--- cl-dmarc/src/file-processor.lisp
+++ cl-dmarc/src/file-processor.lisp
@@ -3,3 +3,17 @@
 (defgeneric lock-next-file (file-processor))
 (defgeneric file-done (file-processor file))
 (defgeneric reset-stuck-files (file-processor))
+
+(se:defclass file-processor ()
+  (pubsub-thread))
+
+(defmethod au:start ((startable file-processor) &rest args)
+  (declare (ignorable args))
+  (format t "start file-processor~%")
+  ;; TODO
+  )
+
+(defmethod au:stop ((startable file-processor))
+  (format t "stop file-processor~%")
+  ;; TODO
+  )
--- cl-dmarc/src/packages/file-processor.lisp
+++ cl-dmarc/src/packages/file-processor.lisp
@@ -1,2 +1,12 @@
 (defpackage :file-processor
-  (:use :cl :arrow-macros :shared))
+  (:use :cl :arrow-macros :shared)
+  (:local-nicknames
+   (:a :alexandria)
+   (:s :serapeum)
+   (:se :serapeum.exporting)
+   (:au :apputils)
+   (:ps :pubsub)
+   (:psr :pubsub.rabbit)
+   ;;(:mi :cl-mime)
+   ;;(:b64 :cl-base64)
+   ))
