d63c77006f05e43b767d72791f4dfc69ccc119df
Oliver Wegner <oliver.wegner@csr-informatik.de>
2025-04-07 14:52:26 +0200

    working on web backend in v

--- web-backend-v/main.v
+++ web-backend-v/main.v
@@ -8,9 +8,25 @@ pub struct Context {
 
 pub struct App {}
 
+struct User {
+  id string
+  lastname string
+  firstname string
+}
+
 fn main() {
   mut app := &App{}
   veb.run[App, Context](mut app, 8081)
+}
+
+@['/api/dmarc/query'; get]
+pub fn (app &App) dmarc_query(mut ctx Context) veb.Result {
+  results := User{
+    '123'
+    'Doe'
+    'John'
+  }
+  return ctx.json(results)
 }
 
 pub fn (app &App) index(mut ctx Context) veb.Result {
