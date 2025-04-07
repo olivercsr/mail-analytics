0d777cddf37cc27b4710be46f016c8baca2ac1a4
Oliver Wegner <oliver.wegner@csr-informatik.de>
2025-04-07 20:37:56 +0200

    playing around with vlang & veb

--- web-backend-v/main.v
+++ web-backend-v/main.v
@@ -1,5 +1,6 @@
 module main
 
+import time
 import veb
 
 pub struct Context {
@@ -21,12 +22,26 @@ fn main() {
 
 @['/api/dmarc/query'; get]
 pub fn (app &App) dmarc_query(mut ctx Context) veb.Result {
-  results := User{
+  println('entering index')
+  time.sleep(5000 * time.millisecond)
+  user := User{
     '123'
     'Doe'
     'John'
   }
-  return ctx.json(results)
+  data := {
+    'foo': 123
+    'bar': 234
+  }
+  result := struct {
+    user: user
+    data: data
+  }
+  println(typeof(user).name)
+  println(typeof(data).name)
+  println(typeof(result).name)
+  println('leaving index')
+  return ctx.json([result])
 }
 
 pub fn (app &App) index(mut ctx Context) veb.Result {
