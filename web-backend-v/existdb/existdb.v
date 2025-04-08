6fde71ac19c82680464b0efd1facb3415cf359c4
Oliver Wegner <oliver.wegner@csr-informatik.de>
2025-04-09 00:10:29 +0200

    add query for fetching rowcount

--- web-backend-v/existdb/existdb.v
+++ web-backend-v/existdb/existdb.v
@@ -23,7 +23,32 @@ pub fn new_existdb(config Config) ExistDb {
 
 pub fn (db ExistDb) query(query string) !string {
   url := '${db.config.baseurl}${api_path}/${db.config.collection}'
-  response := http.get(url)!
+  //response := http.get(url)!
+  q := '<query xmlns="http://exist.sourceforge.net/NS/exist" start="1" max="5" cache="yes" session-id="">
+  <text>
+<![CDATA[
+xquery version "3.1";
+
+for \$row in collection(\'/dmarc\')/feedback/record/row
+    group by \$ip := \$row/source_ip/text()
+    let \$rowCount := count(\$row)
+    let \$totalCount := sum(\$row/count)
+    let \$avgCount := \$totalCount div \$rowCount
+    order by \$ip
+    return
+        <item>
+            <ip>{\$ip}</ip>
+            <rows>{\$rowCount}</rows>
+            <count>{\$totalCount}</count>
+            <avgCount>{\$avgCount}</avgCount>
+        </item>
+]]>
+  </text>
+  <properties>
+    <property name="foo" value="bar"/>
+  </properties>
+</query>'
+  response := http.post(url, q)!
 
   return response.body
 }
