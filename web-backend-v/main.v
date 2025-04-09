642edd8b6a920a3657da0ebd778d8ef9669106f3
Oliver Wegner <oliver.wegner@csr-informatik.de>
2025-04-09 18:57:37 +0200

    working on renderer

--- web-backend-v/main.v
+++ web-backend-v/main.v
@@ -1,56 +1,9 @@
 module main
 
-import time
 import veb
 
 import renderer
 import existdb
-
-pub struct App {}
-
-struct User {
-  id string
-  lastname string
-  firstname string
-}
-
-@['/api/dmarc/query'; get]
-pub fn (app &App) dmarc_query(mut ctx renderer.Context) veb.Result {
-  handler := fn (app &App, mut context renderer.Context) {
-    println('start processing request ${context.req.url}')
-
-    time.sleep(5000 * time.millisecond)
-
-    user := User{
-      '123'
-      'Doe'
-      'John'
-    }
-    data := {
-      'foo': 123
-      'bar': 234
-    }
-    result := struct {
-    user: user
-    data: data
-  }
-
-    println(typeof(user).name)
-    println(typeof(data).name)
-    println(typeof(result).name)
-
-    println('done processing request ${context.req.url}')
-
-    context.json([result])
-  }
-
-  return renderer.process_request_concurrently(handler, app, mut ctx)
-}
-
-pub fn (app &App) index(mut ctx renderer.Context) veb.Result {
-  message := 'Hello world from veb!'
-  return $veb.html()
-}
 
 fn test_existdb() {
   db := existdb.new_existdb(existdb.Config{
@@ -67,7 +20,7 @@ fn test_existdb() {
 fn main() {
   test_existdb()
 
-  mut app := &App{}
-  veb.run[App, renderer.Context](mut app, 8081)
+  mut app := &renderer.App{}
+  veb.run[renderer.App, renderer.Context](mut app, 8081)
 }
 
--- web-backend-v/renderer/renderer.v
+++ web-backend-v/renderer/renderer.v
@@ -1,3 +1,51 @@
 module renderer
 
+import time
+import veb
+
+pub struct App {}
+
+struct User {
+  id string
+  lastname string
+  firstname string
+}
+
+@['/api/dmarc/query'; get]
+pub fn (app &App) dmarc_query(mut ctx renderer.Context) veb.Result {
+  handler := fn (app &App, mut context renderer.Context) {
+    println('start processing request ${context.req.url}')
+
+    time.sleep(5000 * time.millisecond)
+
+    user := User{
+      '123'
+      'Doe'
+      'John'
+    }
+    data := {
+      'foo': 123
+      'bar': 234
+    }
+    result := struct {
+    user: user
+    data: data
+  }
+
+    println(typeof(user).name)
+    println(typeof(data).name)
+    println(typeof(result).name)
+
+    println('done processing request ${context.req.url}')
+
+    context.json([result])
+  }
+
+  return renderer.process_request_concurrently(handler, app, mut ctx)
+}
+
+pub fn (app &App) index(mut ctx renderer.Context) veb.Result {
+  message := 'Hello world from veb!'
+  return $veb.html()
+}
 
