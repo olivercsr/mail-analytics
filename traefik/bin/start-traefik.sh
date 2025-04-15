c7cdc5ff4a8c0a5d83e2f33979747fdd5244135d
Oliver Wegner <oliver.wegner@csr-informatik.de>
2025-04-15 17:34:55 +0200

    playing around with traefik config

--- traefik/bin/start-traefik.sh
+++ traefik/bin/start-traefik.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 
 podman run -it --rm \
-  -p 8080:8080 \
+  -p 8080:80 \
+  -p 8443:443 \
+  -p 8082:8080 \
   -v $(pwd)/traefik.yml:/etc/traefik/traefik.yml:ro \
   -v $(pwd)/file-provider.yml:/etc/traefik/file-provider.yml:ro \
   docker.io/traefik:v3
