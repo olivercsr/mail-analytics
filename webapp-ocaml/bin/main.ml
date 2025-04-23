61ed9645c5c1c40ec7f0fb53240fce2285ed5b33
Oliver Wegner <oliver.wegner@csr-informatik.de>
2025-04-23 16:38:02 +0200

    adding webapp-ocaml

---
+++ webapp-ocaml/bin/main.ml
@@ -0,0 +1,33 @@
+let successful = ref 0
+let failed = ref 0
+
+let count_requests inner_handler request =
+  try%lwt
+    let%lwt response = inner_handler request in
+    successful := !successful + 1;
+    Lwt.return response
+
+  with exn ->
+    failed := !failed + 1;
+    raise exn
+
+let () =
+  Dream.run
+  @@ Dream.logger
+  @@ count_requests
+  @@ Dream.router [
+
+    Dream.get "/fail"
+      (fun _ ->
+        raise (Failure "The web app failed!"));
+
+    Dream.get "/"
+      (fun _ ->
+        Dream.html (Printf.sprintf "successful: %3i, failed: %3i" !successful !failed));
+
+    Dream.get "/echo/:word"
+      (fun request ->
+        Dream.html (Dream.param request "word"));
+
+  ]
+
