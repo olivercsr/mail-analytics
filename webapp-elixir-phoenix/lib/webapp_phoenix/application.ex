9f242e0b3b1e754602b4109edee0145da5e43a4c
Oliver Wegner <oliver.wegner@csr-informatik.de>
2025-05-01 19:03:10 +0200

    adding webapp-elixir-phoenix

---
+++ webapp-elixir-phoenix/lib/webapp_phoenix/application.ex
@@ -0,0 +1,35 @@
+defmodule WebappPhoenix.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      WebappPhoenixWeb.Telemetry,
+      {DNSCluster, query: Application.get_env(:webapp_phoenix, :dns_cluster_query) || :ignore},
+      {Phoenix.PubSub, name: WebappPhoenix.PubSub},
+      # Start the Finch HTTP client for sending emails
+      {Finch, name: WebappPhoenix.Finch},
+      # Start a worker by calling: WebappPhoenix.Worker.start_link(arg)
+      # {WebappPhoenix.Worker, arg},
+      # Start to serve requests, typically the last entry
+      WebappPhoenixWeb.Endpoint
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: WebappPhoenix.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  @impl true
+  def config_change(changed, _new, removed) do
+    WebappPhoenixWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
---
+++ webapp-elixir-phoenix/lib/webapp_phoenix/mailer.ex
@@ -0,0 +1,3 @@
+defmodule WebappPhoenix.Mailer do
+  use Swoosh.Mailer, otp_app: :webapp_phoenix
+end
