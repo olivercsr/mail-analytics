3046ad9cff8ea51df0ec7839e46c9f742fda87c4
Oliver Wegner <oliver.wegner@csr-informatik.de>
2025-05-04 21:15:19 +0200

    working on sample webapp-rust

--- webapp-rust/src/main.rs
+++ webapp-rust/src/main.rs
@@ -3,9 +3,19 @@ use axum::{
     Router,
 };
 
+async fn get_foo() -> &'static str {
+    "Hello foo!"
+}
+
+async fn post_foo() -> &'static str {
+    "Hello foo post!"
+}
+
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello world!" }));
+    let app = Router::new()
+        .route("/", get(|| async { "Hello world!" }))
+        .route("/foo", get(get_foo).post(post_foo));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8081").await.unwrap();
     axum::serve(listener, app).await.unwrap();
