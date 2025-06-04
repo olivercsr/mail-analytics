15c7b96e0cea74b9410d84e0b1bec8aeaddf57d3
Oliver Wegner <oliver.wegner@csr-informatik.de>
2025-06-04 18:38:48 +0200

    working on xquery templating in webapp-elixir-phoenix

--- webapp-elixir-phoenix/lib/webapp_phoenix/existdb.ex
+++ webapp-elixir-phoenix/lib/webapp_phoenix/existdb.ex
@@ -3,9 +3,13 @@ defmodule WebappPhoenix.ExistDb do
   require Logger
 
   def query(query_name) do
-    query = EEx.eval_file("priv/xqueries/query_count.eex", [query_name: query_name])
+    query = EEx.eval_file("priv/xqueries/#{query_name}.eex", [query_name: query_name])
+    xml_query = EEx.eval_file("priv/xqueries/container.eex", [query: query, variables: [
+      [key: "wantedBegin", type: "integer", value: 123]
+    ]])
+
     Logger.info(EEx.eval_string("query_name: <%= query_name %>", [query_name: query_name]))
-    Logger.info("query: #{query}")
+    Logger.info("query: #{xml_query}")
 
     1234
   end
--- webapp-elixir-phoenix/lib/webapp_phoenix_web/controllers/query_controller.ex
+++ webapp-elixir-phoenix/lib/webapp_phoenix_web/controllers/query_controller.ex
@@ -4,7 +4,7 @@ defmodule WebappPhoenixWeb.QueryController do
   def count(conn, %{"start" => startts, "end" => endts} = _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    results = WebappPhoenix.ExistDb.query("count")
+    results = WebappPhoenix.ExistDb.query("query_count")
     IO.puts("results: #{results}")
 
     render(conn, :count, startts: startts, endts: endts)
