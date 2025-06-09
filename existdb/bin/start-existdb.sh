fe9fddad43f8d402cd17825ae41c28cec9852c95
Oliver Wegner <oliver.wegner@csr-informatik.de>
2025-06-09 17:01:16 +0200

    update start-existdb script

--- existdb/bin/start-existdb.sh
+++ existdb/bin/start-existdb.sh
@@ -13,9 +13,10 @@ fi
 EXISTDB_NAME=${EXISTDB_NAME:-existdb-dev}
 
 ${DOCKER_CMD} run -it --rm \
-  --name "${EXISTDB_NAME}" \
+  --name existdb \
   --hostname "${EXISTDB_NAME}.local" \
-  -m 768m \
   -p 8080:8080 \
   -p 8443:8443 \
+  -p 3333:3333 \
+  -e JAVA_TOOL_OPTIONS="-Dfile.encoding=UTF8 -Dsun.jnu.encoding=UTF-8 -Djava.awt.headless=true -Dorg.exist.db-connection.cacheSize=256M -Dorg.exist.db-connection.pool.max=20 -Dlog4j.configurationFile=/exist/etc/log4j2.xml -Dexist.home=/exist -Dexist.configurationFile=/exist/etc/conf.xml -Djetty.home=/exist -Dexist.jetty.config=/exist/etc/jetty/standard.enabled-jetty-configs -XX:+UseG1GC -XX:+UseStringDeduplication -XX:+UseContainerSupport -XX:MaxRAMPercentage=75.0 -XX:+ExitOnOutOfMemoryError -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=3333 -Dcom.sun.management.jmxremote.rmi.port=3333 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false" \
   docker.io/existdb/existdb:release
