acc1ab6881e16e0f50f5e5afd9f00fb5d92f7a9c
Oliver Wegner <oliver.wegner@csr-informatik.de>
2025-06-10 15:23:51 +0200

    adding volume to existdb podman start script

--- existdb/bin/start-existdb.sh
+++ existdb/bin/start-existdb.sh
@@ -19,4 +19,5 @@ ${DOCKER_CMD} run -it --rm \
   -p 8443:8443 \
   -p 3333:3333 \
   -e JAVA_TOOL_OPTIONS="-Dfile.encoding=UTF8 -Dsun.jnu.encoding=UTF-8 -Djava.awt.headless=true -Dorg.exist.db-connection.cacheSize=256M -Dorg.exist.db-connection.pool.max=20 -Dlog4j.configurationFile=/exist/etc/log4j2.xml -Dexist.home=/exist -Dexist.configurationFile=/exist/etc/conf.xml -Djetty.home=/exist -Dexist.jetty.config=/exist/etc/jetty/standard.enabled-jetty-configs -XX:+UseG1GC -XX:+UseStringDeduplication -XX:+UseContainerSupport -XX:MaxRAMPercentage=75.0 -XX:+ExitOnOutOfMemoryError -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=3333 -Dcom.sun.management.jmxremote.rmi.port=3333 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false" \
+  -v existdb-data:/exist/data:Z \
   docker.io/existdb/existdb:release
