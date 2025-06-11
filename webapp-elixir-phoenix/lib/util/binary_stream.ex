b61cc4287a6cbc862eec98c906aaa6205e82941c
Oliver Wegner <oliver.wegner@csr-informatik.de>
2025-06-11 19:36:12 +0200

    adding binary_stream to webapp-elixir-phoenix

---
+++ webapp-elixir-phoenix/lib/util/binary_stream.ex
@@ -0,0 +1,25 @@
+defmodule Util.BinaryStream do
+  defp next_fn(<<head::binary-size(4), rest::binary>>) do
+    IO.puts("next_fn2 #{inspect head}")
+    {:binary.bin_to_list(head), rest}
+  end
+
+  defp next_fn(<<b, rest::binary>>) do
+    IO.puts("next_fn1 #{b}")
+    {[b], rest}
+  end
+
+  defp next_fn(<<>>) do
+    IO.puts("next_fn0")
+    {:halt, <<>>}
+  end
+
+  def new(binary_data) do
+    Stream.resource(
+      fn -> binary_data end,
+      &next_fn/1,
+      fn res -> res end
+    )
+  end
+end
+
