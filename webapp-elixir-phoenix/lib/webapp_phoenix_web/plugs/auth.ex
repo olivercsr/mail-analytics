2b75b9393ed0f688d7eda7e682857ae8ff733b52
Oliver Wegner <oliver.wegner@csr-informatik.de>
2025-06-25 20:40:01 +0200

    adding auth plug

---
+++ webapp-elixir-phoenix/lib/webapp_phoenix_web/plugs/auth.ex
@@ -0,0 +1,40 @@
+defmodule WebappPhoenixWeb.Plugs.Auth do
+  import Plug.Conn
+  require Config
+  require Logger
+
+  def init(default), do: default
+
+  defp get_authuser_from_env() do
+    (Application.get_env(:webapp_phoenix, :dev_authuser, "") || "") |> String.trim()
+  end
+
+  defp get_authuser_from_header(conn, authuser_header) do
+    [authuser] = conn
+      |> Plug.Conn.get_req_header(authuser_header) # TODO: lookup mail addresses
+      |> Enum.map(&String.trim/1)
+
+    authuser
+  end
+
+  defp get_authuser(conn, authuser_header) do
+    case Application.get_env(:webapp_phoenix, :env, :unknown) do
+      :dev -> case get_authuser_from_env() do
+        authuser when is_binary(authuser) and authuser != "" -> authuser
+        _ -> get_authuser_from_header(conn, authuser_header)
+      end
+      _ -> get_authuser_from_header(conn, authuser_header)
+    end
+  end
+
+  def call(conn, authuser_header) do
+    authuser = case get_authuser(conn, authuser_header) do
+      authuser when is_binary(authuser) and authuser != "" -> authuser
+    end
+
+    Logger.info([module: __MODULE__, message: "authuser", authuser: authuser])
+
+    assign(conn, :authuser, authuser)
+  end
+end
+
