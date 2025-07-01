9fc436c7e9aec8e6d29c33f3f7b1bdd2fef0472a
Oliver Wegner <oliver.wegner@csr-informatik.de>
2025-07-01 21:29:04 +0200

    working on query liveview

--- webapp-elixir-phoenix/lib/webapp_phoenix_web/liveviews/query_live.ex
+++ webapp-elixir-phoenix/lib/webapp_phoenix_web/liveviews/query_live.ex
@@ -13,34 +13,26 @@ defmodule WebappPhoenixWeb.QueryLive do
 
   def date_input(assigns) do
     ~H"""
-    <input type="date" name={@field.name} id={@field.id} value={@field.value} />
+    <label class="label">{@field.name}</label>
+    <input type="date" class="input" name={@field.name} id={@field.id} value={@field.value} />
     """
   end
 
-  def mount(%{"start" => startts, "end" => endts} = params, session, socket) do
-    IO.puts("MOUNT")
-    IO.inspect(params)
-    IO.inspect(session)
-
-    # TODO: get tenant from session
-    # TODO: add ui controls to adjust start & end, which will then in turn trigger re-query
-
-    {startts_int, ""} = Integer.parse(startts)
-    {endts_int, ""} = Integer.parse(endts)
-    # existdb_config = Application.get_env(:webapp_phoenix, Db.ExistDb)
-    # [tenant] = Plug.Conn.get_req_header(conn, "remote-user") # TODO: lookup mail addresses
-    #   |> Enum.map(&String.trim/1)
-    # tenant = conn.assigns.authuser
-    # tenant = "dmarc@csr-informatik.de"
-    # tenant = Plug.Conn.get_session(session, :authuser)
-    tenant = Map.fetch!(session, "authuser")
+  defp query(tenant, startdate, enddate) do
+    startts = with {:ok, startdt} <- DateTime.new(startdate, ~T[00:00:00], "Etc/UTC") do
+      DateTime.to_unix(startdt)
+    end
+    endts = with {:ok, enddt} <- DateTime.new(enddate, ~T[00:00:00], "Etc/UTC") do
+      DateTime.to_unix(enddt)
+    end
 
     {:ok, result_stream} = Db.ExistDb.query(Db.ExistDb,
       tenant,
       "query_count",
-      %{wantedBegin: startts_int, wantedEnd: endts_int}
+      %{wantedBegin: startts, wantedEnd: endts}
     )
-    results = result_stream
+
+    result_stream
       |> Stream.map(fn {:item, xml} ->
         xml |> xpath(
           ~x"/item",
@@ -53,27 +45,61 @@ defmodule WebappPhoenixWeb.QueryLive do
         )
       end)
       |> Enum.to_list()
+  end
+
+  defp param_to_date(param) when is_binary(param) do
+    with {i, _} <- Integer.parse(param),
+      {:ok, d} <- DateTime.from_unix(i) do
+      {:ok, DateTime.to_date(d)}
+    end
+  end
+
+  defp param_to_date(param) when is_integer(param) do
+    with {:ok, d} <- DateTime.from_unix(param) do
+      {:ok, DateTime.to_date(d)}
+    end
+  end
+
+  def mount(params, session, socket) do
+    IO.puts("MOUNT")
+    IO.inspect(params)
+    IO.inspect(session)
+
+    # TODO: add ui controls to adjust start & end, which will then in turn trigger re-query
+
+    tenant = Map.fetch!(session, "authuser")
+    startdate = case param_to_date(params["start"]) do
+      {:ok, date} -> date
+      _ -> DateTime.now!("Etc/UTC")
+        |> DateTime.add(-30, :day)
+        |> DateTime.to_date()
+    end
+    enddate = case param_to_date(params["end"]) do
+      {:ok, date} -> date
+      _ -> DateTime.now!("Etc/UTC")
+        |> DateTime.to_date()
+    end
+    # results = query(tenant, ~D[2024-06-01], ~D[2025-03-31])
+    results = query(tenant, startdate, enddate)
 
     {:ok, assign(socket,
       tenant: tenant,
-      form: to_form(%{"start" => startts, "end" => endts}),
-      startts: startts,
-      endts: endts,
-      results: results,
-      myrnd: 0
+      form: to_form(%{"start" => startdate, "end" => enddate}),
+      startdate: startdate,
+      enddate: enddate,
+      results: results
     )}
-  end
-
-  def handle_event("newrnd", params, socket) do
-    IO.puts("HANDLE_EVENT NEWRND")
-    IO.inspect(params)
-    {:noreply, update(socket, :myrnd, fn _ -> IO.puts("update"); :rand.uniform(1000) end)}
   end
 
   def handle_event("change_date", params, socket) do
     IO.puts("HANDLE_EVENT CHANGE_DATE")
     IO.inspect(params)
-    {:noreply, assign(socket, myrnd: :rand.uniform(1000))}
+    IO.inspect(socket)
+    {:ok, startdate} = Date.from_iso8601(params["start"])
+    {:ok, enddate} = Date.from_iso8601(params["end"])
+    tenant = socket.assigns[:tenant]
+    results = query(tenant, startdate, enddate)
+    {:noreply, assign(socket, results: results)}
   end
 end
 
