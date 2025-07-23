98b786ad39f32d94a9dae4da3d60b95e33ec81a2
Oliver Wegner <oliver.wegner@csr-informatik.de>
2025-07-23 17:52:35 +0200

    adding lldap start script

---
+++ lldap/bin/start-lldap.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+podman run -it --rm \
+  -e LLDAP_KEY_SEED=a12345678901234567890 \
+  -e LLDAP_JWT_SECRET=a12345678901234567890 \
+  -e LLDAP_LDAP_BASE_DN=dc=csr-informatik,dc=de \
+  -e LLDAP_LDAP_USER_PASS=foobar123 \
+  -e TZ=Europe/Berlin \
+  -p17170:17170 -p3890:3890 \
+  -v lldap_data:/data:Z \
+  docker.io/lldap/lldap
+
