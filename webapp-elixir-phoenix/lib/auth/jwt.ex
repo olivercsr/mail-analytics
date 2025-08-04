f9de0062f20430cd3b89d237f19258e6746baf33
Oliver Wegner <oliver.wegner@csr-informatik.de>
2025-08-04 20:24:03 +0200

    adding notes

--- webapp-elixir-phoenix/lib/auth/jwt.ex
+++ webapp-elixir-phoenix/lib/auth/jwt.ex
@@ -1,6 +1,10 @@
 defmodule Auth.Jwt do
   use Joken.Config
 
+  # TODO: implement:
+  #  - expiry (copy value from 3rd party token?)
+  #  - ...
+
   @impl true
   def token_config do
     default_claims()
