3da3557e20042d5e4c4847e997466f5c16a897cc
Oliver Wegner <oliver.wegner@csr-informatik.de>
2025-08-05 13:58:32 +0200

    working on jwt

--- webapp-elixir-phoenix/lib/auth/jwt.ex
+++ webapp-elixir-phoenix/lib/auth/jwt.ex
@@ -1,10 +1,20 @@
 defmodule Auth.Jwt do
   use Joken.Config
 
+  defp is_email_address(str) do
+    re = ~r"^([^@\s]+)@.+$"
+    [_all, email] = Regex.run(re, str)
+    case email |> String.trim() do
+      email when is_binary(email) and email != "" and email != nil -> email
+    end
+  end
+
   @impl true
   def token_config do
     default_claims()
-      |> add_claim("sub", nil, fn sub -> String.length(sub) > 5 end)
+      |> add_claim("sub", nil, fn sub -> is_binary(sub) and String.length(sub) > 0 end)
+      |> add_claim("email", nil, &is_email_address/1)
+      |> add_claim("provider", nil, fn provider -> is_binary(provider) and String.length(provider) > 0 end)
   end
 end
 
